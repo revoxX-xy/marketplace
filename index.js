0ea5ae745233f32dbf72dd473e47ee3563399904
revoxX-xy <revox.core@gmail.com>
2026-04-04 02:06:16 +0700

    Add user registration and login endpoints

--- index.js
+++ index.js
@@ -3,10 +3,36 @@ const app = express();
 
 app.use(express.json());
 
+let users = [];
+
+// home
 app.get("/", (req, res) => {
   res.send("Marketplace API jalan 🔥");
 });
 
-app.listen(3000, () => {
-  console.log("Server jalan");
+// register
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  users.push({ username, password });
+
+  res.json({ message: "Register berhasil" });
 });
+
+// login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (user) {
+    res.json({ message: "Login berhasil" });
+  } else {
+    res.json({ message: "Login gagal" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server jalan"));
